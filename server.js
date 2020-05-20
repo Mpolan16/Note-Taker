53e7e1bcae8857cf410a330e577eed182bf36c0f
Mónica Polanco <mpolan16@g.holycross.edu>
2020-05-20 16:03:45 -0400

    added server

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const http = require("http");
+const fs = require("fs");
 
 const PORT = 8080;
 
-function handleRequest(request, response) {
-
-  response.end("It Works!! Path Hit: " + request.url);
+function handleRequest(req, res) {
+    fs.readFile(__dirname + "/public/index.html", function(err, data) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
 }
-
 const server = http.createServer(handleRequest);
-
 server.listen(PORT, function() {
-
   console.log("Server listening on: http://localhost:" + PORT);
 });
