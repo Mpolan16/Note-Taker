329a65a41f1e2f07f1b6be66ba982425404fe68c
Mónica Polanco <mpolan16@g.holycross.edu>
2020-05-20 16:33:09 -0400

    routes working

--- server.js
+++ server.js
@@ -3,14 +3,50 @@ const fs = require("fs");
 
 const PORT = 8080;
 
+const server = http.createServer(handleRequest);
+server.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
+
 function handleRequest(req, res) {
+
+    const path = req.url;
+
+    switch (path) {
+
+    case "/":
+        return displayHome(res);
+    
+    case "/notes":
+        return displayNotes(res);
+    
+    default:
+        return display404(path, res);
+    }
+}
+//create a function for home page
+function displayHome(res){
     fs.readFile(__dirname + "/public/index.html", function(err, data) {
         if (err) throw err;
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(data);
       });
 }
-const server = http.createServer(handleRequest);
-server.listen(PORT, function() {
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+//create func for notes
+function displayNotes(res){
+    fs.readFile(__dirname + "/public/notes.html", function(err, data) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+}
+
+//create func for 404
+function display404(path, res) {
+  const myHTML = "<html>" +
+    "<body><h1>404 Not Found </h1>" +
+    "<p>The page you were looking for: " + path + " can not be found</p>" +
+    "</body></html>";
+  res.writeHead(404, { "Content-Type": "text/html" });
+  res.end(myHTML);
+}
