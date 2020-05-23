266fe7d47657267e803c4e4b8f09aacb1afc285f
Mónica Polanco <mpolan16@g.holycross.edu>
2020-05-23 10:03:01 -0400

    delete note is working

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,4 +1,4 @@
-const fs = require ("fs");
+  const fs = require ("fs");
 const { v4: uuidv4 } = require("uuid");
 
 module.exports = function(app) {
@@ -8,31 +8,47 @@ module.exports = function(app) {
                 throw error
             }
             res.json(JSON.parse(data));
-        })
+        });
     });
   
     app.post("/api/notes", function(req, res) {
-        var post = req.body;
+        let post = req.body;
         post.id = uuidv4(); 
         fs.readFile("db/db.json", "utf8", (error,data) => {
             if(error){
                 throw error
-            }
-            var dataArray = JSON.parse(data)
+            };
+            let dataArray = JSON.parse(data)
             dataArray.push(post);
-            fs.writeFile("db/db.json", JSON.stringify(dataArray), (err)=>{
+            fs.writeFile("db/db.json", JSON.stringify(dataArray), (error) =>{ 
                 if(error){
                     throw error
                 } else {
                     console.log("Write File Successful!")
-                }
-            })
-        })
+                };
+            });
+        });
         res.json(post);
       });
     app.delete("/api/notes/:id", function(req, res) {
-        notesData.push(req.body);
-        res.json(true);
-      });
-    };
+        fs.readFile("db/db.json", "utf8", (error,data) => { 
+            let delNote = req.params.id;
+            let dataArray = 
+            JSON.parse(data).filter(note => {
+                if (note.id === delNote) {
+                    return false
+                } else {
+                    return true
+                }
+            });
+            fs.writeFile("db/db.json", JSON.stringify(dataArray), (error) =>{ 
+                if(error){
+                    throw error
+                } else {
+                    console.log("successfully deleted")
+                    };
+                });
+            })}
+        )}
 
+    
